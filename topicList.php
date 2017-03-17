c03d685dd3709a912fb706bc0e6efb30e10b4e7e
abhilash-47 <abhilashsmacbook@gmail.com>
2017-03-18 00:14:10 +0100

    Topic List

---
+++ topicList.php
@@ -0,0 +1,131 @@
+<?php
+include("inc/connectDB.php");
+
+if(isset($_SESSION["username"])){
+$username=$_SESSION["username"];
+$userinfo=mysql_query("select * from user where username='$username'");
+$user=mysql_fetch_array($userinfo);
+$uid=$user['uid'];
+$name=$user['name'];
+$admin=$user['isadmin'];
+}
+
+$catId=$_GET["cid"];
+
+$query=mysql_query("select * from topic,user where topic.userId=user.uid and categoryId='$catId'");
+$new=array();
+$x=1;
+while($row = mysql_fetch_array($query,MYSQL_ASSOC)){
+        $new[$x] = $row;
+        $x++;
+}
+
+//print_r($new[1]['name']);
+
+require("header.php");
+?>
+
+<script type="text/javascript">
+    function doCheck()
+    {
+        var title = document.getElementById("title");
+        var content = document.getElementById("content")
+        
+        if(title.value=="")
+        {
+            alert("Please enter the title.");
+            return false;
+        }
+
+        if(content.value=="")
+        {
+            alert("Please enter the content.");
+            return false;
+        }
+    }
+</script>
+
+<!-- Start of Page Container -->
+<div class="page-container">
+        <div class="container">
+                <div class="row">
+                <!-- start of page content -->
+                <div class="span8 page-content">
+
+                        <!-- Basic Home Page Template -->
+                        <div class="row separator">
+                                <section class="span8">
+                                        <h3>Featured Articles</h3>
+                                        <ul class="articles">
+                                                <?php
+                                                $i = 1;
+
+                                                for($i = 1; $i < $x; $i++)
+                                                {
+                                                ?>
+                                                <li class="article-entry standard">
+                                                        <h4><a href="single.html"><?php echo $new[1]['title']; ?></a></h4>
+                                                        <span class="article-meta"><?php echo $new[1]['topTime']; ?> - <a href="#"><?php echo $new[1]['name']; ?></a></span>
+                                                        
+                                                </li>
+                                                <?php
+                                                }
+                                                ?>
+                                                
+                                                <br>
+                                                
+                                                <li id="pagination">
+                                                        <a href="#" class="btn active">1</a>
+                                                        <a href="#" class="btn">2</a>
+                                                        <a href="#" class="btn">3</a>
+                                                        <a href="#" class="btn">Next »</a>
+                                                        <a href="#" class="btn">Last »</a>
+                                                </li>
+                                        </ul>
+                                        
+                                </section>
+
+                                <section class="span8">
+                                        <div id="respond">
+
+                                                <h3>New Topic</h3>
+
+                                                <form action="inc/newTopic.php" method="post" id="newTopic" onsubmit="return doCheck()">
+
+                                                        <div>
+                                                                <label for="title">Title</label>
+                                                                <input class="span8" type="text" name="title" id="title" value="" size="22" >
+                                                        </div>
+
+
+                                                        <div>
+                                                                <label for="content">Content</label>
+                                                                <textarea class="span8" name="content" id="content" cols="58" rows="10"></textarea>
+                                                        </div>
+
+                                                        <p class="allowed-tags">You can use these HTML tags and attributes <small><code>&lt;a href="" title=""&gt; &lt;abbr title=""&gt; &lt;acronym title=""&gt; &lt;b&gt; &lt;blockquote cite=""&gt; &lt;cite&gt; &lt;code&gt; &lt;del datetime=""&gt; &lt;em&gt; &lt;i&gt; &lt;q cite=""&gt; &lt;strike&gt; &lt;strong&gt; </code></small></p>
+
+                                                        <div>
+                                                                <input class="btn" name="submit" type="submit" id="submit"  value="Submit">
+                                                                <input class="btn" name="reset" type="reset" id="reset" value="Reset">
+                                                        </div>
+
+                                                </form>
+
+                                        </div>
+                                </section>
+                        </div>
+
+                </div>
+                <!-- end of page content -->
+
+                <?php require("sidebar.php"); ?>
+
+                </div>
+        </div>
+</div>
+<!-- End of Page Container -->
+
+<?php
+require("footer.php");
+?>
